61b3b4da1769bb4a2f30b1073618591a922e15ec
Palendran <106523236+Palendran@users.noreply.github.com>
2022-07-07 08:28:20 +0530

    Add files via upload

---
+++ myproject/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Focus Engneering company</title>
+  <link href="focus\css\style.css" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+
+    
+   
+    <nav>
+    <img class="logo" src="focus\images\logo.png" alt="logo" >
+      <ul class="nav_link">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+      <a><button class="btn">Login</button></a>
+</nav>
+</div> 
+  
+
+
+
+<div class="body1">
+
+<h1 class="heading1">We Focus On Your Success...</h1>
+<p class="p1">Provide better services to the clients with the maximum <br>
+satisfaction through quality work and cost effective <br>
+by introducing new technologie <br>
+<a><button class="btn2" >About<span>More</span> </button></a>
+</p>
+
+</div>
+<div class="container">
+ <div class="service">
+<h1 class="heading2">- Our Best Services</h1>
+</div>
+<div class="row">
+  <div class="col-md-3 text-center"></div>  
+  <div >
+    <img class="image1" src="focus\images\img1.jpeg" alt="image1">
+  </div>
+  
+</div>
+<div class="col-md-3 text-center"></div>  
+  <div >
+    <img class="image1" src="focus\images\img1.jpeg" alt="image1">
+  </div>
+  
+</div>
+
+
+</div>
+</body>
+</html>
